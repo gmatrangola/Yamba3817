5d2cda1819ac79fc037f7f7ae81ca2fec305ef8e
Geoff Matrangola <geoff@matrangola.com>
2014-11-05 10:35:05 -0800

    Reading preferences

--- app/src/main/java/com/thenewcircle/yamba/YambaPostService.java
+++ app/src/main/java/com/thenewcircle/yamba/YambaPostService.java
@@ -5,7 +5,9 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.marakana.android.yamba.clientlib.YambaClient;
@@ -34,28 +36,46 @@ public class YambaPostService extends IntentService {
         Notification.Builder builder = new Notification.Builder(this);
         builder.setSmallIcon(R.drawable.ic_launcher);
         builder.setContentTitle("Yamba");
-        builder.setContentText("Sending " + status);
-        builder.setLights(Color.rgb(255, 0,0), 500, 200);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String username = prefs.getString("userName", null);
+        String password = prefs.getString("password", null);
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        manager.notify(NOTIFICATION_ID, builder.getNotification());
-        final YambaClient client = new YambaClient("student", "passwordX");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            Log.wtf(TAG, "interrupt");
-        }
-        try {
-            client.postStatus(status);
-            manager.cancel(NOTIFICATION_ID);
-        } catch (YambaClientException e) {
-            Log.e(TAG, "Unable to post " + status, e);
-            builder.setContentText("Error posting: " + status + " " + e.getMessage());
-            Intent activity = new Intent(this, PostActivity.class);
-            activity.putExtra("status", status);
-            PendingIntent pending = PendingIntent.getActivity(this, 0, activity, 0);
+
+        if(username == null || password == null) {
+            builder.setContentText("No username/password. Press here.");
+            builder.setAutoCancel(true);
+            Intent settingsActivity = new Intent(this, SettingsActivity.class);
+            PendingIntent pending = PendingIntent.getActivity(this, 0, settingsActivity, 0);
             builder.setContentIntent(pending);
             manager.notify(NOTIFICATION_ID, builder.getNotification());
         }
+        else {
+            builder.setContentText("Sending " + status);
+            builder.setLights(Color.rgb(255, 0,0), 500, 200);
+            manager.notify(NOTIFICATION_ID, builder.getNotification());
+            final YambaClient client = new YambaClient(username, password);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                Log.wtf(TAG, "interrupt");
+            }
+            try {
+                client.postStatus(status);
+                manager.cancel(NOTIFICATION_ID);
+            } catch (YambaClientException e) {
+                Log.e(TAG, "Unable to post " + status, e);
+                builder.setContentText("Error posting: " + status + " " + e.getMessage());
+                Intent activity = new Intent(this, PostActivity.class);
+                activity.putExtra("status", status);
+                PendingIntent pending = PendingIntent.getActivity(this, 0, activity, 0);
+                builder.setContentIntent(pending);
+                manager.notify(NOTIFICATION_ID, builder.getNotification());
+            }
+
+        }
+
+
 
     }
 }
